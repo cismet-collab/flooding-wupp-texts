5c35cd38bbc907ccdd12e5fe61f621cf35d9e70c
Stefan Sander <stefan.sander@t-online.de>
2023-12-19 07:54:23 +0000

    Abschnitt Wasserstand abfragen

--- src/lib/help/Help50WasserstandAbfragen.js
+++ src/lib/help/Help50WasserstandAbfragen.js
@@ -23,37 +23,28 @@ const Component = () => {
             Wassertiefe" zunächst mit einem kurzen Bedienungshinweis.
           </p>
           <p>
-            Ein Klick auf eine beliebige Position in der Karte bewirkt jetzt,
-            dass die Zelle in der Karte markiert und der zugehörige Maximalwert
-            der Wassertiefe bzw. der Fließgeschwindigkeit in diesem Feld
-            angezeigt wird. Die Anzeige der maximalen Wassertiefen wird dabei
-            auf volle 10 cm gerundet (z. B. "ca. 20 cm"), um eine mögliche{" "}
-            <a onClick={() => setAppMenuActiveMenuSection("aussagekraft")}>
-              Varianz der Simulationsergebnisse
-            </a>{" "}
-            zu verdeutlichen. Aus demselben Grund werden berechnete Wassertiefen
-            von mehr als 150 cm nur als "> 150 cm" angezeigt. Bitte beachten
-            Sie, dass in der online-Karte Wassertiefen kleiner als 5 cm
-            transparent dargestellt sind, um die Aufmerksamkeit auf die
-            kritischeren Bereiche zu lenken. Überflutungsgefahren können auch in
-            anderen Bereichen vorliegen. Lokale Verhältnisse sind immer zu
-            prüfen (inkl. der Grundstücksentwässerungsanlage). Die Anzeige der
-            maximalen Fließgeschwindigkeiten erfolgt in der Einheit "Meter pro
-            Sekunde" (m/s), gerundet auf eine Nachkommastelle.
-            Fließgeschwindigkeiten von mehr als 6 Meter pro Sekunde werden als
-            "> 6 m/s" angezeigt.
+            Ein Klick auf eine beliebige Position in der Karte bewirkt jetzt, 
+            dass die Zelle in der Karte markiert und der zugehörige Maximalwert 
+            der Wassertiefe in diesem Feld angezeigt wird. Die Anzeige der maximalen 
+            Wassertiefe wird dabei auf volle 10 cm gerundet (z. B. "ca. 50 cm"), 
+            um die beschränkte Aussagekraft der Simulationsergebnisse zu verdeutlichen. 
+            Aus demselben Grund werden berechnete Wassertiefen von mehr als 150 cm nur 
+            als "> 150 cm" angezeigt.
           </p>
           <p>
-            Auch im Abfragemodus können Sie die Karte mit gedrückter linker
-            Maustaste verschieben. Wenn Sie auf diese Weise oder durch{" "}
+            Wenn Sie nach der Abfrage einer maximalen Wassertiefe eine andere 
+            Simulation auswählen, wird der angezeigte Zellwert automatisch 
+            aktualisiert. So können Sie für eine bestimmte Position bequem alle 
+            angebotenen Simulations-Szenarien durchgehen. Auch im Abfragemodus 
+            können Sie die Karte mit gedrückter linker Maustaste verschieben. 
+            Wenn Sie auf diese Weise oder durch{" "}
             <a onClick={() => setAppMenuActiveMenuSection("positionieren")}>
               Positionierung über einen Suchbegriff
             </a>{" "}
-            einen Kartenausschnitt auswählen, in dem die zuletzt abgefragte
-            Zelle nicht mehr enthalten ist, wird das Anzeigefeld auf seinen
-            Startzustand zurückgesetzt. Mit einem Klick auf das{" "}
-            <Icon name="close" /> Symbol rechts oben im Anzeigefeld beenden Sie
-            den Abfragemodus.
+            einen Kartenausschnitt auswählen, wird das Anzeigefeld 
+            auf seinen Startzustand zurückgesetzt. Mit einem Klick auf das{" "}
+            <Icon name="close" /> Symbol rechts oben im Anzeigefeld 
+            beenden Sie den Abfragemodus.
           </p>
         </div>
       }

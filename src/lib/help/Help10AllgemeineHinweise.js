92561d16d4194654a9c5dac83bc12fc6891396e3
Stefan Sander <stefan.sander@t-online.de>
2024-04-04 14:16:13 +0000

    Abschnitt Hintergrund erster Teil

--- src/lib/help/Help10AllgemeineHinweise.js
+++ src/lib/help/Help10AllgemeineHinweise.js
@@ -11,64 +11,51 @@ const Component = () => {
 
   return (
     <GenericModalMenuSection
-      sectionKey="allgemeineHinweise"
-      sectionTitle="Allgemeine Hinweise"
+      sectionKey="hintergrund"
+      sectionTitle="Hintergrund"
       sectionBsStyle="warning"
       sectionContent={
         <div>
-          Platzhaltertext
           <p>
-            Die TopicMap Hochwasser stellt in drei umschaltbaren Kartenansichten
-            maximale Wassertiefen bzw. maximale Fließgeschwindigkeiten im
-            gesamten Gültigkeitsgebiet dar, die im Verlauf von drei simulierten
-            Starkregenszenarien berechnet wurden. Bei den Ergebnissen handelt es
-            sich um eine modelltechnische Untersuchung des Gültigkeitsgebiets
-            mit dem Ziel die Realität möglichst gut abzubilden. Bei der
-            Betrachtung und Interpretation der Ergebnisse sind die
-            Modellgrundlagen und -annahmen zu berücksichtigen. Hinweise zur
-            Aussagekraft der Ergebnisse werden nachfolgend beschrieben.
+            Die Stadt Wuppertal publiziert seit Ende 2018 eine interaktive 
+            Starkregengefahrenkarte, mit deren Hilfe alle Nutzer/innen die Gefahr 
+            einer Überflutung ihrer Grundstücke durch Regenwasser einschätzen 
+            können, das im Verlauf von Starkregenereignissen auf der Erdoberfläche 
+            abfließt. Als Datengrundlage verwendet die Starkregengefahrenkarte die 
+            Ergebnisse von Simulationsberechnungen, die im Auftrag der Stadt Wuppertal 
+            und der Wuppertaler Stadtwerke (WSW Energie und Wasser AG) durch das 
+            Ingenieurbüro Dr. Pecher AG (Erkrath) durchgeführt wurden.
           </p>
           <p>
-            Das Ergebnis der Simulation ist von der Dauer und Intensität des
-            Regens abhängig, der für die Simulation angenommen wird. Wir bieten
-            Ihnen hierzu drei unterschiedliche{" "}
-            <a
-              className="renderAsLink"
-              onClick={() => setAppMenuActiveMenuSection("szenarien")}
-            >
-              simulierte Szenarien
-            </a>{" "}
-            an, darunter ein außergewöhnlicher Starkregen (SRI 7) und ein
-            extremer Starkregen (SRI 9 bzw. 10) als "Modellregen" sowie die
-            Abbildung der Niederschlagsbelastung des Ereignisses vom 14.07 und
-            15.07.2021, basierend auf Radardaten.{" "}
+            Bei den katastrophalen Überschwemmungen am 14. und 15. Juli 2021, die   
+            vor allem im Ahrtal verheerende Schäden angerichtet und viele   
+            Menschenleben gekostet haben, war auch die Stadt Wuppertal betroffen. Es 
+            handelte sich um ein kombiniertes Starkregen- und Flusshochwasserereignis, 
+            das von Simulationen, wie sie für die Starkregengefahrenkarte gemacht 
+            werden, nicht abgebildet wird. Simulationen für Flusshochwasser werden 
+            in einem Turnus von sechs Jahren vom Land NRW für die sog. Risikogewässer 
+            durchgeführt. In Wuppertal sind das neben der Wupper die Schwelme, der 
+            Mirker Bach, der Morsbach, der Hardenberger Bach und der Deilbach. Auch 
+            wenn es keine gängigen Simulationsansätze für kombinierte Starkregen- 
+            und Flusshochwasserereignisse gibt, kann doch mit einer parallelen 
+            Darstellung der Gefährdung durch Starkregenabfluss und Flusshochwasser 
+            ein vollständiges Bild der Überflutungsgefährdung vermittelt werden. 
+            Die AG "Gefahrenkarte", eine Unterarbeitsgruppe der vom Wuppertaler 
+            Oberbürgermeister nach dem Hochwasser vom Juli 2021 eingerichteten 
+            Taskforce "Hochwasser" hat daher beschlossen, die Hochwassersimulationen 
+            des Landes NRW für das Stadtgebiet von Wuppertal in einer 
+            Hochwassergefahrenkarte zugänglich zu machen, die sich in Aussehen und 
+            Bedienung  an der Starkregengefahrenkarte orientiert. Außerdem sollte 
+            die neue Hochwassergefahrenkarte mit der Starkregengefahrenkarte so 
+            gekoppelt werden können, dass die Nutzer/innen bei der gleichzeitigen 
+            Betrachtung der Gefährdungen durch Starkregen und Hochwasser bestmöglich 
+            unterstützt werden.  
           </p>
           <p>
             Der Farbverlauf für die <strong>Wassertiefen</strong> nutzt die
             Eckwerte 20 cm (blau), 40 cm (gelb), 75 cm (orange) und {">"} 100 cm
             (rot). Wassertiefen unter 5 cm werden nicht mehr farbig ausgeprägt
             (transparente Darstellung).
-          </p>
-          <p>Die Simulationsberechnungen wurden durch ... durchgeführt.</p>
-          <p>
-            Die Bemessungsgrenze des Kanalnetzes wird bei den hier simulierten
-            Ereignissen überschritten. Der größte Anteil des Niederschlagwassers
-            fließt oberirdisch ab, wobei im urbanen Raum sowohl die
-            Leistungsfähigkeit des Kanalnetzes als auch der Überstau des
-            Kanalnetzes einen Einfluss auf die Überflutungssituation haben kann.
-            Das Abflussverhalten des Kanalnetzes wurde durch eine kombinierte
-            Betrachtung berücksichtigt. Die Eigenschaften der Geländeoberfläche
-            variieren je nach Flächennutzung. Die verschiedenen
-            Oberflächeneigenschaften wurden im Modell über unterschiedliche
-            Geländerauheiten und zeitlich variable Versickerungsansätze
-            abgebildet. Relevante Retentionsräume sowie Gewässerverrohrungen und
-            Durchlässe wurden aufgrund ihrer Bedeutung für die Ableitungs- und
-            Speicherwirkung von Oberflächenwasser als Elemente in das Modell
-            aufgenommen. Je nach Regen und Randbedingungen in der Realität
-            können in Teilgebieten die Wassertiefen auf dem Gelände höher oder
-            geringer ausfallen. Um diese Variationen zu verstehen und besser
-            einschätzen zu können, sind die unterschiedlichen Szenarien
-            dargestellt.
           </p>
         </div>
       }

69c04f3b1559f2545fed0dd86d4ee0bfc41a1f23
Stefan Sander <stefan.sander@t-online.de>
2024-04-05 07:52:38 +0000

    Abschnitt zur Koppelung mit SRGK eingefügt

---
+++ src/lib/help/Help35KoppelungMitStarkregengefahrenkarte.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Icon from "react-cismap/commons/Icon";
+import GenericModalMenuSection from "react-cismap/topicmaps/menu/Section";
+
+const Component = () => {
+  return (
+    <GenericModalMenuSection
+      sectionKey="koppelung"
+      sectionTitle="Koppelung mit Starkregengefahrenkarte"
+      sectionBsStyle="success"
+      sectionContent={
+        <div>
+          <p>
+            2Do
+          </p>
+        </div>
+      }
+    />
+  );
+};
+export default Component;
